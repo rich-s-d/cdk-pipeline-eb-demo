099b336dcae004c6499e958b009f0285a02f9fa2
Shane Rich <srichnz@gmail.com>
2023-09-11 20:53:50 +0200

    Configuration of cdk pipeline under way.

--- bin/cdk-pipeline-eb-demo.ts
+++ bin/cdk-pipeline-eb-demo.ts
@@ -1,21 +1,27 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkPipelineEbDemoStack } from '../lib/cdk-pipeline-eb-demo-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { CdkPipelineStack } from "../lib/cdk-pipeline-stack";
+import * as AWS from "aws-sdk";
 
 const app = new cdk.App();
-new CdkPipelineEbDemoStack(app, 'CdkPipelineEbDemoStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// Create an AWS Secrets Manager client
+const secretsManager = new AWS.SecretsManager({ region: "us-west-1" }); // Specify the appropriate region
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+// Fetch the AWS account ID from Secrets Manager
+secretsManager.getSecretValue({ SecretId: "AWSAccountID" }, (err: any, data: any) => {
+  if (err) {
+    console.error("Error fetching AWS account ID from Secrets Manager:", err);
+    process.exit(1);
+  }
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  const account = JSON.parse(data.SecretString).AWSAccountID;
+
+  // Create the CDK stack with the retrieved account ID
+  new CdkPipelineStack(app, "CdkPipelineStack", {
+    env: { account, region: "eu-west-1" },
+  });
+
+  app.synth();
 });
