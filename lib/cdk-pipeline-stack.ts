28f61d19e6f13328348f1a9372cbf0e7dd04a2e9
Shane Rich <srichnz@gmail.com>
2023-09-12 18:09:56 +0200

    Added github auth to pipeline.

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -1,13 +1,40 @@
 import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
-import { Stack, StackProps } from "aws-cdk-lib";
+import { SecretValue, Stack, StackProps } from "aws-cdk-lib";
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
 
-/**
- * The stack that defines the application pipeline
- */
+// /**
+//  * The stack that defines the application pipeline
+//  */
+// export class CdkPipelineStack extends Stack {
+//   constructor(scope: Construct, id: string, props?: StackProps) {
+//     super(scope, id, props);
+
+//     const pipeline = new CodePipeline(this, "Pipeline", {
+//       // The pipeline name
+//       pipelineName: "MyServicePipeline",
+
+//       // How it will be built and synthesized
+//       synth: new ShellStep("Synth", {
+//         // Where the source can be found
+//         input: CodePipelineSource.gitHub("rich-s-d/cdk-pipeline-eb-demo", "main"),
+
+//         // Install dependencies, build and run cdk synth
+//         installCommands: ["npm i -g npm@latest"],
+//         commands: ["npm ci", "npm run build", "npx cdk synth"],
+//       }),
+//     });
+
+//     // This is where we add the application stages
+//   }
+// }
+
 export class CdkPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // Define the AWS Secrets Manager secret containing your GitHub token
+    const githubTokenSecret = Secret.fromSecretNameV2(this, "GitHubTokenSecret", "github-token-v3");
 
     const pipeline = new CodePipeline(this, "Pipeline", {
       // The pipeline name
@@ -16,7 +43,9 @@ export class CdkPipelineStack extends Stack {
       // How it will be built and synthesized
       synth: new ShellStep("Synth", {
         // Where the source can be found
-        input: CodePipelineSource.gitHub("rich-s-d/cdk-pipeline-eb-demo", "main"),
+        input: CodePipelineSource.gitHub("rich-s-d/cdk-pipeline-eb-demo", "main", {
+          authentication: SecretValue.secretsManager(githubTokenSecret.secretArn),
+        }),
 
         // Install dependencies, build and run cdk synth
         installCommands: ["npm i -g npm@latest"],
